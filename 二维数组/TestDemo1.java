299ab91e9ba4714814d81ce9a7fcf79590207b1f
GangSugar <74159930+GangSugar@users.noreply.github.com>
2021-02-19 10:45:03 +0800

    Add files via upload

---
+++ 二维数组/TestDemo1.java
@@ -0,0 +1,17 @@
+package 玩转数组习题.二维数组;
+
+public class TestDemo1 {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+        for (int row = 0;row < arr.length;row++){
+            for (int col = 0;col < arr[row].length;col++){
+                System.out.printf("%d\t",arr[row][col]);
+            }
+            System.out.println("");
+        }
+    }
+}
